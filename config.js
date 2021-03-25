5fbb51adeaf1b0d63cba0b7f88198774f370a7dd
Boris Gautier <tchoukouahaboris@outlook.fr>
2021-03-25 15:52:00 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ module.exports = {
     path_style_qml_default:'./python_script/style_default/',
     path_style_qml_template:'./python_script/style_template/',
     path_style_qml:'/var/www/geosm/style/',
-    url_qgis_server:'http://tiles.geocameroun.xyz/cgi-bin/qgis_mapserv.fcgi?map=',
+    url_qgis_server:'https://tiles.geocameroun.xyz/cgi-bin/qgis_mapserv.fcgi?map=',
     url_node_js:'http://service.geocameroun.cm/',
 }
